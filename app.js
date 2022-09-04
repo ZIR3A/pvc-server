5d58d0123004de90aa0881fca70283783b6a6a56
dev.saran <zireavai8@gmail.com>
2022-09-04 21:50:07 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4001;
+
+const app = express();
+
+// app.use(express.static('../client/build'));
+// app.get("*", (req, res) => {
+//     res.sendFile(require('path')
+//         .resolve(__dirname, 'client', 'build', 'index.html'));
+// })
+// app.get('/', (req,res)=>{
+//     res.send("hello")
+// })
+
+
+const server = http.createServer(app);
+
+// const io = socketIo(server);
+var io = socketIo(server,{
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+  });
+// let interval;
+
+io.on("connection", (socket) => {
+  socket.emit('connection-success', {
+    status: 'connection-success',
+    socketId: socket.id,
+  })
+
+  socket.on('sdp', data => {
+    socket.broadcast.emit('sdp', data)
+  })
+
+  // get data from fromtend
+  socket.on('candidate', data => {
+    socket.broadcast.emit('candidate', data)
+  })
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
